101bd867a6122f6d1a74485b9cff6ef13c691479
abizar0016 <muhammadabizar0016@gmail.com>
2024-03-18 21:41:21 +0700

    fix codingan typo

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,12 @@
 <?php
 //KONEKSI KE MYSQLi
-$host="localhost";
-$user="root";
-$pass="";
-$database="myweb";
-$conn=mysqli_connect($host,$user,$pass,$database);
-if(!$conn){
-echo "KONEKSI GAGAL!!";
-}else {
-//echo "KONEKSI BERHASIL";//Komen jika koneksi sudah berhasil
+$host = "localhost";
+$user = "root";
+$pass = "";
+$database = "myweb";
+$conn = mysqli_connect($host, $user, $pass, $database);
+if (!$conn) {
+    echo "KONEKSI GAGAL!!";
+} else {
+    //echo "KONEKSI BERHASIL";//Komen jika koneksi sudah berhasil
 }
-?>
